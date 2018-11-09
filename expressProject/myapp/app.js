c3319406c1677089f36c4b5867b4d50fdf88277f
Yunsol <c21154035@gmail.com>
2018-11-09 16:29:04 +0900

    미들웨어 예시 추가

--- expressProject/myapp/app.js
+++ expressProject/myapp/app.js
@@ -56,4 +56,46 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/*
+middleware1은 요청객체를 이용해 인증 여부를 검사하는 미들웨어입니다. 
+인증이 되었으면 다음 로직을 수행하기 위해 next()를 호출하게 되고 
+그렇지 않을 경우 next() 함수에 에러 인스턴스를 만들어 넘겨줍니다.
+*/
+app.use(function middleware1(req, res, next) {
+  console.log('middleware1: 인증작업...')
+  if (/*인증 되었으면*/true) {
+    next();
+  } else {
+    next(new Error('Unauthorized'))
+  }
+});
+
+/*
+인증에 통과하면 middlware2를 수행하게 될 것입니다.
+이것은 단순히 로깅 작업만 수행한 뒤 next()를 호출합니다.
+*/
+app.use(function middleware2(req, res, next) {
+  console.log('middleware2: 로깅작업...')
+  next();
+});
+
+/*
+middleware3는 좀 특별한 미들웨어입니다. 파라매터를 보면 첫번째 에러객체 err가 추가된 것을 볼 수 있을 겁니다.
+이것은 에러를 처리하는 미들웨어의 인터페이스인데, next(에러객체)를 호출하게 되면 이 에러 미들웨어가 동작하게 됩니다.
+
+미들웨어의 실행 순서를 정리하자면 다음과 같습니다
+
+인증에 성공할 경우: middleware1 -> middleware2 -> 라우팅 로직 -> 응답
+인증에 실패할 경우: middleware1 -> middleware3 -> 응답
+*/
+app.use(function middleware3(err, req, res, next) {
+  if (err) {
+    if (err.message === 'Unauthorized') {
+      return res.status(401).send('Unauthorized');
+    }
+    return res.satus(400).send('BadRequest');
+  }
+  next()
+});
+
 module.exports = app;
