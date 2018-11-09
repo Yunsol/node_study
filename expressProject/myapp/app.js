92af0c9cbe3fe8affc1b99fa35e76147e2d069ee
Yunsol <c21154035@gmail.com>
2018-11-09 15:29:33 +0900

    주석 추가

--- expressProject/myapp/app.js
+++ expressProject/myapp/app.js
@@ -8,6 +8,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// 미리 구현한 라우팅 모듈을 가져온다.
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -33,6 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// URL에 따라 라우팅 모듈을 설정한다.
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
